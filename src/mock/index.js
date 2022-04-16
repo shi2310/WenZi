651549d4aa921b676c5938965b2e4738898a4c79
shi2310 <zhuoyang1121@163.com>
2022-04-16 21:00:46 +0800

    mockjs修复

--- src/mock/index.js
+++ src/mock/index.js
@@ -3,6 +3,16 @@ import Mock from 'mockjs'
 
 const random = Mock.Random
 
+const translateUrl = function (url) {
+    let arr = url.split("?")[1].split("&"); //先通过？分解得到？后面的所需字符串，再将其通过&分解开存放在数组里
+    let obj = {};
+    for (let i of arr) {
+        obj[i.split("=")[0]] = i.split("=")[1]; //对数组每项用=分解开，=前为对象属性名，=后为属性值
+    }
+    return obj;
+};
+
+
 // 拦截ajax请求，配置mock的数据
 Mock.mock(/\/api\/login/, 'post', (req) => {
     const {
@@ -24,28 +34,24 @@ Mock.mock(/\/api\/login/, 'post', (req) => {
 const dataTmp = {
     ID: '@increment',
     LastDate: '@datetime',
-    Total: '@natural',
-    HasPay: '@natural',
-    NeedPay: '@natural',
-    InPay: '@natural',
+    Total: '@natural(60, 100)',
+    HasPay: '@natural(60, 100)',
+    NeedPay: '@natural(60, 100)',
+    InPay: '@natural(60, 100)',
     PayCome: random.string(),
-    Balance: '@natural',
+    Balance: '@natural(60, 100)',
 }
 
 // 拦截ajax请求，配置mock的数据
-Mock.mock(/\/api\/list/, 'get', () => {
-    console.log(req)
-    const {
-        pageSize
-    } = JSON.parse(req.body)
-
+Mock.mock(/\/api\/list/, 'get', (req) => {
+    const { pageSize } = translateUrl(req.url);
     const List = []
     for (let i = 0; i < pageSize; i++) {
         List.push(Mock.mock(dataTmp))
     }
     const data = {
         List,
-        TotalCount:45
+        TotalCount: 45
     }
     return {
         Success: true,
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="head">
-      <el-form
-        :inline="true"
-        :model="searchForm"
-      >
+      <el-form :inline="true" :model="searchForm">
         <el-form-item>
           <el-date-picker
             v-model="searchForm.time"
@@ -13,26 +10,11 @@
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-select
-            v-model="searchForm.type"
-            placeholder="下拉选择"
-          >
-            <el-option
-              label="选项一"
-              value="选项一"
-            ></el-option>
-            <el-option
-              label="选项二"
-              value="选项二"
-            ></el-option>
-            <el-option
-              label="选项三"
-              value="选项三"
-            ></el-option>
-            <el-option
-              label="选项四"
-              value="选项四"
-            ></el-option>
+          <el-select v-model="searchForm.type" placeholder="下拉选择">
+            <el-option label="选项一" value="选项一"></el-option>
+            <el-option label="选项二" value="选项二"></el-option>
+            <el-option label="选项三" value="选项三"></el-option>
+            <el-option label="选项四" value="选项四"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
@@ -42,87 +24,39 @@
           ></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button
-            type="primary"
-            @click="search"
-            icon="el-icon-search"
-          >查询</el-button>
+          <el-button type="primary" @click="search" icon="el-icon-search"
+            >查询</el-button
+          >
         </el-form-item>
       </el-form>
       <div>
-        <el-button
-          type="primary"
-          icon="el-icon-plus"
-        >新增</el-button>
+        <el-button type="primary" icon="el-icon-plus">新增</el-button>
       </div>
     </div>
-    <el-table
-      :data="tableData"
-      border
-      style="width: 100%"
-    >
-      <el-table-column
-        prop="ID"
-        label="编号"
-        width="80"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="LastDate"
-        label="截至日期"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="Total"
-        label="累计结算金额"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="HasPay"
-        label="已支付金额"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="NeedPay"
-        label="应付账款"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="InPay"
-        label="累计入资金池金额"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="PayCome"
-        label="付费来源"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="Balance"
-        label="资金池余额"
-      >
-      </el-table-column>
-      <el-table-column
-        align="center"
-        label="操作"
-      >
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="ID" label="编号" width="50"> </el-table-column>
+      <el-table-column prop="LastDate" label="截至日期"> </el-table-column>
+      <el-table-column prop="Total" label="累计结算金额"> </el-table-column>
+      <el-table-column prop="HasPay" label="已支付金额"> </el-table-column>
+      <el-table-column prop="NeedPay" label="应付账款"> </el-table-column>
+      <el-table-column prop="InPay" label="入资金池金额"> </el-table-column>
+      <el-table-column prop="PayCome" label="付费来源"> </el-table-column>
+      <el-table-column prop="Balance" label="资金池余额"> </el-table-column>
+      <el-table-column align="center" label="操作">
         <template slot-scope="scope">
           <el-button
             @click="handleClick(scope.row)"
             type="text"
             size="small"
             icon="el-icon-view"
-          >查看</el-button>
-          <el-button
-            type="text"
-            size="small"
-            icon="el-icon-edit"
-          >编辑</el-button>
-          <el-button
-            type="text"
-            size="small"
-            icon="el-icon-delete"
-          >删除</el-button>
+            >查看</el-button
+          >
+          <el-button type="text" size="small" icon="el-icon-edit"
+            >编辑</el-button
+          >
+          <el-button type="text" size="small" icon="el-icon-delete"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -130,41 +64,46 @@
 </template>
 
 <script>
-import _ from 'lodash'
+import _ from "lodash";
 export default {
-  name: 'home',
+  name: "home",
   data() {
     return {
       searchForm: {
-        time: '',
-        type: '',
-        text: ''
+        time: "",
+        type: "",
+        text: "",
       },
       tableData: [],
-    }
+    };
   },
   created() {
-    this.$myaxios.get(`/api/list`).then((response) => {
-      const data = response.data
-      if (data && data.Success) {
-        const list = data.Data && data.Data.List
-        _.each(list, item => {
-          this.tableData.push(item)
-        })
-      } else {
-        this.$message.error(data.Msg);
-      }
-    })
+    this.$myaxios
+      .get(`/list`, { params: { pageSize: 10 } })
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        if (data && data.Success) {
+          const list = data.Data && data.Data.List;
+          _.each(list, (item) => {
+            this.tableData.push(item);
+          });
+        } else {
+          this.$message.error(data.Msg);
+        }
+      });
   },
   methods: {
     handleClick(row) {
       console.log(row);
     },
     search() {
-      console.log(`欲提交的数据   日期:${this.searchForm.time}  下拉条件:${this.searchForm.type}  文本: ${this.searchForm.text}`)
-    }
+      console.log(
+        `欲提交的数据   日期:${this.searchForm.time}  下拉条件:${this.searchForm.type}  文本: ${this.searchForm.text}`
+      );
+    },
   },
-}
+};
 </script>
 
 <style scoped lang="less">
